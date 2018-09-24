27b30b77959ffb35982a7cfcd34b36c8c45f2b52
golam-mohammd-shovon <38292502+golam-shovon@users.noreply.github.com>
2018-09-24 20:45:58 +0600

    Up3
    70 percent complte

--- lt_1.cpp
+++ lt_1.cpp
@@ -51,12 +51,45 @@ glVertex2i(150, 50);
 glEnd();
 glFlush ();
 }
+void allforone(void)
+{
+glClear (GL_COLOR_BUFFER_BIT);
+glColor3f (1.0, 0.0, 0.0);
+glPointSize(5.0);
+
+glBegin(GL_LINE_LOOP);
+glVertex2i(100, 50);
+glVertex2i(100, 130);
+glVertex2i(150, 130);
+glVertex2i(150, 50);
+glEnd();
+
+glBegin(GL_POLYGON);
+glVertex2i(100, 150);
+glVertex2i(100, 230);
+glVertex2i(150, 230);
+glVertex2i(150, 150);
+glEnd();
+
+glBegin(GL_TRIANGLES);
+glVertex2i(100, 350);
+glVertex2i(100, 430);
+glVertex2i(150, 430);
+glEnd();
+
+glBegin(GL_LINE_LOOP);
+glVertex2i(100, 50);
+glVertex2i(190, 130);
+glVertex2i(190, 130);
+glEnd();
+glFlush ();
+}
 void myInit (void)
 {
 glClearColor(1.0, 1.0, 1.0, 0.0);
 glMatrixMode(GL_MODELVIEW);
 glLoadIdentity();
-gluOrtho2D(0.0, 240.0, 0.0, 480.0);
+gluOrtho2D(0.0, 640.0, 0.0, 480.0);
 }
  main(int argc, char** argv)
 {
@@ -65,26 +98,32 @@ glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
 
 glutInitWindowSize (240, 480);
 glutInitWindowPosition (200, 150);
-glutCreateWindow ("TRaingle Filled");
+glutCreateWindow ("Triangle Filled");
 glutDisplayFunc(trainglefilled);
 myInit ();
 
 glutInitWindowSize (240,480);
 glutInitWindowPosition (500, 150);
-glutCreateWindow ("Traingle Outlined");
+glutCreateWindow ("Triangle Outlined");
 glutDisplayFunc(traingleoutlined);
 myInit ();
 
 glutInitWindowSize (240,480);
 glutInitWindowPosition (800, 150);
-glutCreateWindow ("Rectanle Outlined");
+glutCreateWindow ("Rectangle Outlined");
 glutDisplayFunc(rectangleoutlined);
 myInit ();
 
 glutInitWindowSize (240,480);
 glutInitWindowPosition (1100, 150);
-glutCreateWindow ("Rectanle Filled");
+glutCreateWindow ("Rectangle Filled");
 glutDisplayFunc(rectanglefilled);
+myInit ();
+
+glutInitWindowSize (640,480);
+glutInitWindowPosition (1600, 150);
+glutCreateWindow ("All For One");
+glutDisplayFunc(allforone);
 myInit ();
 
 glutMainLoop();
