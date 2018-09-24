a26ca740a1b6dc44a3d3b39472f764361115b4fa
golam-mohammd-shovon <38292502+golam-shovon@users.noreply.github.com>
2018-09-24 20:29:14 +0600

    U2
    Traingles Rectangles Completed

--- lt_1.cpp
+++ lt_1.cpp
@@ -24,6 +24,33 @@ glVertex2i(150, 130);
 glEnd();
 glFlush ();
 }
+void rectanglefilled(void)
+{
+glClear (GL_COLOR_BUFFER_BIT);
+glColor3f (1.0, 0.0, 0.0);
+glPointSize(5.0);
+glBegin(GL_POLYGON);
+glVertex2i(100, 50);
+glVertex2i(100, 130);
+glVertex2i(150, 130);
+glVertex2i(150, 50);
+glEnd();
+glFlush ();
+}
+
+void rectangleoutlined(void)
+{
+glClear (GL_COLOR_BUFFER_BIT);
+glColor3f (1.0, 0.0, 0.0);
+glPointSize(5.0);
+glBegin(GL_LINE_LOOP);
+glVertex2i(100, 50);
+glVertex2i(100, 130);
+glVertex2i(150, 130);
+glVertex2i(150, 50);
+glEnd();
+glFlush ();
+}
 void myInit (void)
 {
 glClearColor(1.0, 1.0, 1.0, 0.0);
@@ -35,6 +62,7 @@ gluOrtho2D(0.0, 240.0, 0.0, 480.0);
 {
 glutInit(&argc, argv);
 glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+
 glutInitWindowSize (240, 480);
 glutInitWindowPosition (200, 150);
 glutCreateWindow ("TRaingle Filled");
@@ -45,9 +73,20 @@ glutInitWindowSize (240,480);
 glutInitWindowPosition (500, 150);
 glutCreateWindow ("Traingle Outlined");
 glutDisplayFunc(traingleoutlined);
-
-
 myInit ();
+
+glutInitWindowSize (240,480);
+glutInitWindowPosition (800, 150);
+glutCreateWindow ("Rectanle Outlined");
+glutDisplayFunc(rectangleoutlined);
+myInit ();
+
+glutInitWindowSize (240,480);
+glutInitWindowPosition (1100, 150);
+glutCreateWindow ("Rectanle Filled");
+glutDisplayFunc(rectanglefilled);
+myInit ();
+
 glutMainLoop();
 
 }
