1256cb1ee3a76c09b23a7e6b2b26be9ad216dca8
golam-mohammd-shovon <38292502+golam-shovon@users.noreply.github.com>
2018-10-02 02:24:43 +0600

    still same but the co ordinates were changed

--- lt2__batman.cpp
+++ lt2__batman.cpp
@@ -63,8 +63,9 @@ glVertex2i(396, 360);
 glColor3f (1.0, 1.0, 1.0);
 glEnd();
 
+glColor3f (0.0, 0.0, 0.0);
 glBegin(GL_POLYGON);
-glVertex2i(513, 376);
+/*glVertex2i(513, 376);
 glVertex2i(558, 331);
 glVertex2i(612, 368);
 glVertex2i(656, 333);
@@ -96,12 +97,42 @@ glVertex2i(522, 215);
 glVertex2i(522, 198);
 glVertex2i(504, 196);
 glVertex2i(442, 225);
+glVertex2i(442, 332);*/
+
 glVertex2i(442, 332);
-
-
+glVertex2i(442, 225);
+glVertex2i(504, 196);
+glVertex2i(522, 198);
+glVertex2i(522, 215);
+glVertex2i(504, 217);
+glVertex2i(504, 241);
+glVertex2i(512, 241);
+glVertex2i(512, 252);
+glVertex2i(539, 248);
+glVertex2i(541, 242);
+glVertex2i(566, 239);
+glVertex2i(566, 172);
+glVertex2i(576, 169);
+glVertex2i(576, 195);
+glVertex2i(630, 195);
+glVertex2i(630, 170);
+glVertex2i(637, 170);
+glVertex2i(640, 244);
+glVertex2i(674, 244);
+glVertex2i(675, 249);
+glVertex2i(702, 251);
+glVertex2i(703, 241);
+glVertex2i(719, 420);
+glVertex2i(702, 195);
+glVertex2i(720, 180);
+glVertex2i(764, 214);
+glVertex2i(765, 305);
+glVertex2i(720, 367);
+glVertex2i(656, 333);
+glVertex2i(612, 368);
+glVertex2i(558, 331);
+glVertex2i(513, 376);
 glEnd();
-
-
 
 glFlush ();
 
