9502169976d309304c3ba699a8268e80cfd7265c
golam-mohammd-shovon <38292502+golam-shovon@users.noreply.github.com>
2018-10-08 00:32:55 +0600

    nothing happended ..

--- lt_3_alogrithom.cpp
+++ lt_3_alogrithom.cpp
@@ -6,78 +6,40 @@
 float xx1,xx2,yy1,yy2;
 void dda()
 {
-float x1,x2,y1,y2,dx,dy,m,x,y,xend,yend;
+float x1,x2,y1,y2,length,xincre,yincre,i,x,y;
 x1=xx1;
 x2=xx2;
 y1=yy1;
 y2=yy2;
 glClear (GL_COLOR_BUFFER_BIT);
-glColor3f (1.0, 0.0, 0.0);
-dx=x2-x1;
-dy=y2-y1;
-m=(abs(dx/dy));
-if(m<=1)
+glColor3f (1.0, 1.0, 0.0);
+length=abs(x2-x1);
+if(abs(y2-y1)>length)
 {
-    if(dx>0)
-    {
-        x=round(x1);
-        y=y1;
-        xend=round(x2);
-    }
-    else
-    {
-        x=round(x2);
-        y=y2;
-        xend=round(x1);
-    }
-
-while(x<=xend)
-{
+    length=abs(y2-y1);
+}
+xincre=(x2-x1)/length;
+yincre=(y2-y1)/length;
+x=x+0.5;
+y=y+0.5;
+i=1;
 glPointSize(5.0);
-glBegin(GL_POINTS);
-glVertex2i(x, y);
+glBegin(GL_LINE_LOOP);
+while(i<=length)
+{
+   glVertex2i(trunc(x),trunc(y));
+   x=x+xincre;
+   y=y+yincre;
+}
 glEnd();
 glFlush ();
-float yf=yf+m;
-y=round(yf);
-x++;
-}
-
-}
-if(m>1)
-{
-    if(dy>0)
-    {
-        y=y1;
-        x=x1;
-        yend=round(y2);
-    }
-    else
-    {
-        y=y2;
-        x=x2;
-        yend=round(y1);
-    }
-while(y<=yend)
-{
-glPointSize(5.0);
-glBegin(GL_POINTS);
-glVertex2i(x, y);
-glEnd();
-glFlush ();
-float xf=xf+(1/m);
-x=round(xf);
-y++;
-}
-}
-
 }
 void myInit (void)
 {
 glClearColor(1.0, 1.0, 1.0, 0.0);
 glMatrixMode(GL_MODELVIEW);
 glLoadIdentity();
-gluOrtho2D(0.0, 640.0, 0.0, 480.0);
+gluOrtho2D(0.0, 1920.0, 0.0, 1080.0);
 }
  main(int argc, char** argv)
 {
@@ -87,7 +49,7 @@ glutInit(&argc, argv);
 glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
 printf("1.DDA \n2. Bresenham\n3.exit\n");
 scanf("%d",&n);
-glutInitWindowSize (640, 480);
+glutInitWindowSize (1920, 1080);
 glutInitWindowPosition (200, 150);
 glutCreateWindow ("Plotted");
 if(n==1)
@@ -97,6 +59,7 @@ if(n==1)
     printf("enter end point");
     scanf("%f%f",&xx2,&yy2);
     glutDisplayFunc(dda);
+    myInit();
 
 }
 /*if(n==2)
